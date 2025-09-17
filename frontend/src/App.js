94cf24cdd04c7c9204506bd5addc53a0b6aa57e9
srutip04 <subuhishruti75@gmail.com>
2025-09-17 18:51:20 +0530

    refactor App component to implement routing for login, signup, and event management pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Events from "./pages/Events";
+import AdminEvents from "./pages/AdminEvents";
 
 function App() {
   return (
-    <div className="App">
-      <h1 class="text-4xl font-bold underline">Hello world!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Events />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/admin/events" element={<AdminEvents />} />
+    </Routes>
   );
 }
 
