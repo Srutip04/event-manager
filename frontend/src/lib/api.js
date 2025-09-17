d96bf2d87ddf8b89c99fb0f377eefc2d6317be41
srutip04 <subuhishruti75@gmail.com>
2025-09-17 18:51:49 +0530

    add api module for axios instance and authentication token management

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const API_BASE = "http://127.0.0.1:8000";
+
+export const api = axios.create({ baseURL: API_BASE });
+
+export function setAuthToken(token) {
+  if (token) api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  else delete api.defaults.headers.common["Authorization"];
+}
